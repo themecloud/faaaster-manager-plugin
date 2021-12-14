c1e127cb6cd6065a23348cc3519c189be8cd045d
youssef <me@bnkd.me>
2021-12-14 13:26:16 +0100

    init

---
+++ env.php
@@ -0,0 +1,42 @@
+<?php
+/* define('TENWEB_SITE_URL', "https://10web.io");
+define('TENWEB_DASHBOARD', "https://my.10web.io");
+define('TENWEB_API_URL', 'https://manager.10web.io/api');
+define('TENWEB_S3_BUCKET', '10web-products-production');
+define('TENWEB_MANAGER_ID', 51);
+
+global $tenweb_services;
+
+$tenweb_services = array(
+  'optimizer.10web.io',
+  'security.10web.io',
+  'seo.10web.io',
+  'backup.10web.io',
+  'manager.10web.io',
+  'core.10web.io',
+  'lxd.10web.io'
+);
+
+global $tenweb_regions;
+
+$tenweb_regions = array(
+    "asia-east1" => "Changhua County, Taiwan",
+    "asia-northeast1" => "Tokyo, Japan",
+    "asia-south1" => "Mumbai, India",
+    "asia-southeast1" => "Jurong West, Singapore",
+    "australia-southeast1" => "Sydney, Australia",
+    "europe-north1" => "Hamina, Finland",
+    "europe-west1" => "St. Ghislain, Belgium",
+    "europe-west2" => "London, England, UK",
+    "europe-west3" => "Frankfurt, Germany",
+    "europe-west4" => "Eemshaven, Netherlands",
+    "northamerica-northeast1" => "Montréal, Québec, Canada",
+    "southamerica-east1" => "São Paulo, Brazil",
+    "us-central1" => "Council Bluffs, Iowa, USA",
+    "us-east1" => "Moncks Corner, South Carolina, USA",
+    "us-east4" => "Ashburn, Northern Virginia, USA",
+    "us-west1" => "The Dalles, Oregon, USA",
+    "us-west2" => "Los Angeles, California, USA"
+);
+
+*/
